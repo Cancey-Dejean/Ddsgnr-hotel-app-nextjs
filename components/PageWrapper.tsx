08c6059f52c90d6517d420a7f2adb21a227cd713
Cancey Dejean <canceyd@gmail.com>
2023-12-01 15:06:25 -0600

    removed motion on page transitions

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,32 +1,19 @@
-"use client";
-import { ReactNode } from "react";
-import { motion } from "framer-motion";
-import { twMerge } from "tailwind-merge";
+"use client"
+import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 const PageWrapper = ({
   children,
   className,
 }: {
-  children: ReactNode;
-  className?: string;
+  children: ReactNode
+  className?: string
 }) => {
   return (
-    <motion.main
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-      }}
-      exit={{
-        opacity: 0,
-      }}
-      className={twMerge("flex-1", className)}
-      role="main"
-    >
+    <main className={twMerge("flex-1", className)} role="main">
       {children}
-    </motion.main>
-  );
-};
+    </main>
+  )
+}
 
-export default PageWrapper;
+export default PageWrapper
