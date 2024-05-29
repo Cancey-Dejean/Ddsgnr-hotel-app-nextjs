211357e6fa1d2253f7de11c48e8ad64378588608
Cancey Dejean <canceyd@gmail.com>
2024-05-28 23:49:22 -0500

    refactor: Update CardRoom component to improve image rendering and layout

--- components/CardRoom/index.tsx
+++ components/CardRoom/index.tsx
@@ -8,32 +8,26 @@ import { PortableText } from "next-sanity"
 import { CardRoomProps } from "@/types/CardRoomProps"
 import { Button } from "../ui/button"
 
-const CardRoom = ({
-  featured,
-  image,
-  imageAlt,
-  title,
-  description,
-  price,
-  className,
-  buttonLabel,
-  roomReference,
-}: CardRoomProps) => {
+const CardRoom = ({ featured, className, roomReference }: CardRoomProps) => {
   return (
     <div
       className={cn(
-        "group flex mx-auto  flex-col items-center justify-between overflow-hidden rounded-[10px] border-[0.5px] border-color-border",
-        // featured ? "lg:max-w-[400px]" : "lg:max-w-[900px]",
+        "group flex w-full  flex-col items-center justify-between overflow-hidden rounded-[10px] border-[0.5px] border-color-border",
         className
       )}
     >
-      <div className={cn("overflow-hidden w-full flex-1")}>
+      <div
+        className={cn(
+          "relative overflow-hidden max-h-[488px] flex-shrink-0",
+          featured ? " max-h-[488px]" : "min-h-[187px]"
+        )}
+      >
         <Image
           src={roomReference.image || ""}
           width={640}
           height={488}
           className={cn(
-            "w-full h-full object-cover lg:group-hover:scale-110 transition-all duration-300 ease-in-out"
+            "h-full object-cover lg:group-hover:scale-110 transition-all duration-300 ease-in-out"
           )}
           alt={roomReference.imageAlt || "room image"}
         />
@@ -41,15 +35,14 @@ const CardRoom = ({
 
       <div
         className={cn(
-          "flex w-full flex-col justify-center",
-          roomReference.featured ? "px-6 py-6" : "px-8 py-8 sm:px-12"
+          "flex h-full flex-col px-8 py-8 justify-center",
+          featured ? "px-12 py-6" : " px-6 py-6"
         )}
       >
         <div className="flex flex-col">
           <SectionTitle
             className={cn(
-              "text-[22px] leading-[32px]",
-              roomReference.featured
+              featured
                 ? "text-[40px] leading-[48px]"
                 : "text-[22px] leading-[32px]"
             )}
@@ -57,23 +50,18 @@ const CardRoom = ({
             {roomReference.title}
           </SectionTitle>
 
-          <div
-            className={cn(
-              "w-full",
-              roomReference.featured ? "mt-2 text-[15px]" : "mt-6"
-            )}
-          >
+          <div className={cn("mt-2 text-[15px]")}>
             <PortableText value={roomReference.description} />
           </div>
         </div>
 
         <div
           className={cn(
-            "flex items-center gap-4",
-            roomReference.featured ? "mt-6" : "mt-8"
+            " items-center gap-4 mt-6",
+            featured ? "flex" : "grid grid-cols-2"
           )}
         >
-          <Button>${roomReference.price}/night</Button>
+          <Button>${roomReference.price}/Night</Button>
           <Button>{roomReference.buttonLabel || "View room"}</Button>
         </div>
       </div>
--- components/Rooms/index.tsx
+++ components/Rooms/index.tsx
@@ -16,10 +16,18 @@ const Rooms = ({ rooms }: { rooms?: CardRoomProps[] }) => {
         />
 
         {rooms && (
-          <div className="mx-auto max-w-[900px] grid grid-cols-1 gap-6 sm:grid-cols-2">
-            {rooms.map((room, index) => (
-              <CardRoom key={index} {...room} />
-            ))}
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+            <div className="col-start-1 col-span-1">
+              {rooms.slice(0, 1).map((room, index) => (
+                <CardRoom featured key={index} {...room} className="h-full" />
+              ))}
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
+              {rooms.slice(1, 6).map((room, index) => (
+                <CardRoom key={index} {...room} />
+              ))}
+            </div>
           </div>
         )}
       </Wrapper>
